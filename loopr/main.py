7ab37e991b48cc32ac919a58cf091a90c525ef42
Aastha Sharma <aastha.sharma@inviz.ai>
2020-10-15 11:34:50 +0530

    [LPR-764][Aastha] added paired dataset creation.

--- loopr/main.py
+++ loopr/main.py
@@ -1,6 +1,5 @@
 # tmp file
 # just for testing purpose
-
 from loopr.client import LooprClient
 
 if __name__ == "__main__":
@@ -10,6 +9,11 @@ if __name__ == "__main__":
     dataset =loopr_client.create_dataset(type="image",name="test-data9", slug="test-data100" )
     print(dataset)
     dataset.delete()
+    paired_type = {'query': 'text', 'data': 'image'}
+    dataset1 = loopr_client.create_dataset(type="paired", name="text-image-dataa", slug="text-image-dataa", paired_type=paired_type)
+    print(dataset1)
+    dataset1.delete()
+
     datasets = loopr_client.get_datasets()
     for data in datasets:
         print(data)
